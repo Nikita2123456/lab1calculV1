c3395f252c996cb6da263e2f4fce346266e015ce
Nikita2123456 <savosinnekita2gmail.com>
2025-03-09 16:29:27 +0800

    добавлены функции возведения в степень и квадратный корень

--- lab1calculV1/Program.cs
+++ lab1calculV1/Program.cs
@@ -15,37 +15,51 @@ namespace lab1calculV1
             Console.WriteLine("2. Вычитание");
             Console.WriteLine("3. Умножение");
             Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Возведение в степень");
+            Console.WriteLine("6. Квадратный корень");
 
             string choice = Console.ReadLine();
 
-            Console.Write("Введите первое число: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Введите второе число: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
             double result = 0;
 
-            switch (choice)
+            if (choice == "6")
             {
-                case "1":
-                    result = num1 + num2;
-                    break;
-                case "2":
-                    result = num1 - num2;
-                    break;
-                case "3":
-                    result = num1 * num2;
-                    break;
-                case "4":
-                    if (num2 != 0)
-                        result = num1 / num2;
-                    else
-                        Console.WriteLine("Ошибка: деление на ноль");
-                    break;
-                default:
-                    Console.WriteLine("Неверный ввод");
-                    return;
+                Console.Write("Введите число: ");
+                double num = Convert.ToDouble(Console.ReadLine());
+                result = Math.Sqrt(num);
+            }
+            else
+            {
+                Console.Write("Введите первое число: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case "1":
+                        result = num1 + num2;
+                        break;
+                    case "2":
+                        result = num1 - num2;
+                        break;
+                    case "3":
+                        result = num1 * num2;
+                        break;
+                    case "4":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                            Console.WriteLine("Ошибка: деление на ноль");
+                        break;
+                    case "5":
+                        result = Math.Pow(num1, num2);
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        return;
+                }
             }
 
             Console.WriteLine($"Результат: {result}");
